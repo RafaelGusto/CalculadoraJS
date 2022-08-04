2031b3c3518ca94fa2781364ed0e4596e197259c
RafaelGusto <rafael@ambientelivre.com.br>
2022-08-04 16:27:50 -0300

    getters e setters data e hora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,9 @@ class CalcController {
 
     constructor(){
 
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl =  document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._displayCalc = "0";
         this._currentDate;
         this.initialize();
@@ -10,31 +13,50 @@ class CalcController {
 
     initialize(){
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl =  document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
 
-        displayCalcEl.innerHTML = "456";
-        dateEl.innerHTML = "04/08/2022";
-        timeEl.innerHTML = "14:42";
+
+
+    }
+
+    get displayTime(){
+        
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value){
+        
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate(){
+        
+        return this._dateEl.innerHTML;
+
+    }
+
+    set displayDate(value){
+        
+        return this._dateEl.innerHTML = value;
 
     }
 
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
 
     }
 
     set displayCalc(valor){
         
-        this._displayCalc = valor;
+        this._displayCalcEl.innerHTML = valor;
 
     }
 
     get currentDate(){
 
-        return this._currentDate;
+        return new Date();
 
     }
 
